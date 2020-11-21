336e190d319a971112884314fd9ce5f48679f72b
Vejae McLean <vejaem@yahoo.com>
2020-11-21 12:46:03 -0500

    Continued editing

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody=d3.select("tbody");
+data.forEach((ufosighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufosighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+var buttonCol = d3.select("#filter-btn")
+var dateCol = d3.select("#datetime")
+var stateCol = d3.select("#state")
+var countryCol = d3.select("#country")
+var shapeCol = d3.select("#shape")
